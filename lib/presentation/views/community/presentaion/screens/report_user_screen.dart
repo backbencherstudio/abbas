479f4c65bd90a18ce1cd7f198fabea3950889ecb
nasir-uddin-dev <nasiruddin3954@gmail.com>
2026-04-14 17:15:12 +0600

    Inside elevated button, call report api method

--- lib/presentation/views/community/presentaion/screens/report_user_screen.dart
+++ lib/presentation/views/community/presentaion/screens/report_user_screen.dart
@@ -1,9 +1,17 @@
+import 'package:abbas/cors/routes/route_names.dart';
+import 'package:abbas/cors/utils/app_utils.dart';
+import 'package:abbas/presentation/views/community/presentaion/provider/community/community_screen_provider.dart';
 import 'package:abbas/presentation/widgets/secondary_appber.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+import '../../../profile/view_model/profil_screen_provider.dart';
 
 class ReportUserScreen extends StatefulWidget {
-  const ReportUserScreen({super.key});
+  final String reason;
+
+  const ReportUserScreen({super.key, required this.reason});
 
   @override
   State<ReportUserScreen> createState() => _ReportUserScreenState();
@@ -14,6 +22,9 @@ class _ReportUserScreenState extends State<ReportUserScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final profileProvider = Provider.of<ProfileScreenProvider>(context);
+    final data = profileProvider.otherProfileModel?.data;
+    final userId = data?.id ?? "N/A";
     return Scaffold(
       backgroundColor: Color(0xff030C15),
       body: Column(
@@ -22,6 +33,7 @@ class _ReportUserScreenState extends State<ReportUserScreen> {
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 20.w),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(height: 24.h),
 
@@ -46,7 +58,7 @@ class _ReportUserScreenState extends State<ReportUserScreen> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        "title",
+                        'Reason for Reporting',
                         style: TextStyle(
                           fontSize: 14.sp,
                           color: Color(0xff8C9196),
@@ -55,7 +67,7 @@ class _ReportUserScreenState extends State<ReportUserScreen> {
                       ),
                       SizedBox(height: 8.h),
                       Text(
-                        "content",
+                        widget.reason,
                         style: TextStyle(
                           fontSize: 16.sp,
                           fontWeight: FontWeight.w500,
@@ -64,6 +76,15 @@ class _ReportUserScreenState extends State<ReportUserScreen> {
                     ],
                   ),
                 ),
+                Text(
+                  "Reason",
+                  style: TextStyle(
+                    fontSize: 12.sp,
+                    color: Color(0xFFB2B5B8),
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(height: 6.h),
                 Container(
                   padding: EdgeInsets.all(16.w),
                   margin: EdgeInsets.only(bottom: 24.h),
@@ -97,7 +118,37 @@ class _ReportUserScreenState extends State<ReportUserScreen> {
                 SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      final message = _messageController.text;
+                      final res = await context
+                          .read<CommunityScreenProvider>()
+                          .report(
+                            reason: widget.reason,
+                            description: message,
+                            userId: userId,
+                          );
+
+                      if (res.success == true) {
+                        Utils.showToast(
+                          msg: res.message,
+                          backgroundColor: Colors.green,
+                          textColor: Colors.white,
+                        );
+                        if (context.mounted) {
+                          Navigator.pushNamedAndRemoveUntil(
+                            context,
+                            RouteNames.parentScreen,
+                            (route) => false,
+                          );
+                        }
+                      } else {
+                        Utils.showToast(
+                          msg: res.message,
+                          backgroundColor: Colors.red,
+                          textColor: Colors.white,
+                        );
+                      }
+                    },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.white,
                       foregroundColor: Color(0xff030C15),
@@ -106,13 +157,17 @@ class _ReportUserScreenState extends State<ReportUserScreen> {
                       ),
                       padding: EdgeInsets.symmetric(vertical: 16.h),
                     ),
-                    child: Text(
-                      'Submit Report',
-                      style: TextStyle(
-                        fontSize: 16.sp,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
+                    child:
+                        context.watch<CommunityScreenProvider>().isLoading ==
+                            true
+                        ? const CircularProgressIndicator(color: Colors.black)
+                        : Text(
+                            'Submit Report',
+                            style: TextStyle(
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
                   ),
                 ),
               ],
