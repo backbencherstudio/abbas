6ebd49e836fa085bb2fdcd1ec9008baeac2f06b1
Mosharof Khan <mosharof5khan@gmail.com>
2026-05-07 10:57:13 +0600

    Update

--- lib/cors/constants/api_endpoints.dart
+++ lib/cors/constants/api_endpoints.dart
@@ -1,7 +1,7 @@
 class ApiEndpoints {
   ApiEndpoints._();
 
-  static const String baseUrl = 'http://192.168.7.14:4000';
+  static const String baseUrl = 'http://192.168.7.12:4000';
   static const String register = '$baseUrl/api/auth/register';
   static const String login = '$baseUrl/api/auth/login';
   static const String refreshToken = '$baseUrl/api/auth/refresh-token';
